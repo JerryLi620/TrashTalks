e763a2317b797f0d61c20db0da11e27e62bf85e1
Jerry Li <lmyjerry@gmail.com>
2023-02-10 20:53:06 -0500

    First Commit

---
+++ paper-kit-react-main/src/index.js
@@ -0,0 +1,60 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.3.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+// styles
+import "bootstrap/scss/bootstrap.scss";
+import "assets/scss/paper-kit.scss?v=1.3.0";
+import "assets/demo/demo.css?v=1.3.0";
+// pages
+import Index from "views/Index.js";
+import NucleoIcons from "views/NucleoIcons.js";
+import LandingPage from "views/examples/LandingPage.js";
+import ProfilePage from "views/examples/ProfilePage.js";
+import RegisterPage from "views/examples/RegisterPage.js";
+// others
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/index" render={(props) => <Index {...props} />} />
+      <Route
+        path="/nucleo-icons"
+        render={(props) => <NucleoIcons {...props} />}
+      />
+      <Route
+        path="/landing-page"
+        render={(props) => <LandingPage {...props} />}
+      />
+      <Route
+        path="/profile-page"
+        render={(props) => <ProfilePage {...props} />}
+      />
+      <Route
+        path="/register-page"
+        render={(props) => <RegisterPage {...props} />}
+      />
+      <Redirect to="/index" />
+    </Switch>
+  </BrowserRouter>
+);
