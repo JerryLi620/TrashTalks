34e607c2c38555490d6b5e59015c7291a6ec08a8
Jerry Li <lmyjerry@gmail.com>
2023-02-10 23:37:29 -0500

    Create web main page skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Button, Container } from "reactstrap";
 
+// return (
+//   <div className="App">
+//     <div id="photoInput">
+//       <input type="file" id="fileInput" accept="image/*" />
+//       <button onclick="document.getElementById('fileInput').click();">
+//         Upload Image
+//       </button>
+//     </div>
+//     <div id="result">
+//       s<h1>result:</h1>
+//     </div>
+//   </div>
+// );
 function App() {
+  let pageHeader = React.createRef();
+
+  const buttonOnClick = () => {
+    document.getElementById("fileInput").click();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundImage: "url(" + require("./assets/background.jpg") + ")",
+      }}
+      className="page-header"
+      data-parallax={true}
+      ref={pageHeader}
+    >
+      <div className="filter" />
+      <Container>
+        <div className="motto text-center">
+          <h1>Name</h1>
+          <h3>Upload Image:</h3>
+          <br />
+
+          <Button
+            onClick={buttonOnClick}
+            className="btn-round mr-1"
+            color="neutral"
+            target="_blank"
+            outline
+          >
+            <input
+              type="file"
+              id="fileInput"
+              accept="image/*"
+              style={{ width: 0, height: 0 }}
+            />
+            Upload
+          </Button>
+        </div>
+      </Container>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// styles
+import "bootstrap/scss/bootstrap.scss";
+import "./assets/scss/paper-kit.scss?v=1.3.0";
+import "./assets/demo/demo.css?v=1.3.0";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
